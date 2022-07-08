d25574bf5ce5555dddda72875a647e6ece5632b4
Pinghigh <77083712+Pinghigh@users.noreply.github.com>
2022-07-08 08:02:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 FILE="$(basename "$0")"
 
 # Enable the multilib repository
-echo -e '\n[multilib]\nInclude = /etc/pacman.d/mirrorlist\n[archlinuxcn]\nServer = https://mirrors.bfsu.edu.cn/archlinuxcn/$arch\nSigLevel = Never' | tee -a /etc/pacman.conf
+echo -e '\n[multilib]\nInclude = /etc/pacman.d/mirrorlist\n\n[archlinuxcn]\nServer = https://mirrors.xtom.us/archlinuxcn/$arch\nServer = https://mirrors.ocf.berkeley.edu/archlinuxcn/$arch\nServer = https://mirrors.aliyun.com/archlinuxcn/$arch\nSigLevel = Never' | tee -a /etc/pacman.conf
 pacman -Syu --noconfirm --needed base-devel
 sudo pacman -Sy && sudo pacman -S archlinuxcn-keyring  --noconfirm
 # Makepkg does not allow running as root
